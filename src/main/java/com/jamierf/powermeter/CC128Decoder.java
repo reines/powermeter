d18f4d9f9fbd47bc859743f51fc5281c97711b0c
Jamie Furness <jamie@jamierf.co.uk>
2012-02-19 01:58:28 +0000

    Adding sernsor information to readings

--- src/main/java/com/jamierf/powermeter/CC128Decoder.java
+++ src/main/java/com/jamierf/powermeter/CC128Decoder.java
@@ -3,10 +3,14 @@ package com.jamierf.powermeter;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.thoughtworks.xstream.XStream;
 
 class CC128Decoder extends StringDecoder {
+
+	private static final Logger logger = LoggerFactory.getLogger(CC128Decoder.class);
 
 	@SuppressWarnings("unused")
 	private static class Message {
@@ -38,7 +42,10 @@ class CC128Decoder extends StringDecoder {
 		if (xml == null)
 			return null;
 
+		if (logger.isTraceEnabled())
+			logger.trace(xml);
+
 		final Message reading = (Message) xstream.fromXML(xml);
-		return new Reading(reading.tmpr, reading.ch1.watts);
+		return new Reading(reading.sensor, reading.tmpr, reading.ch1.watts);
 	}
 }
--- src/main/java/com/jamierf/powermeter/Reading.java
+++ src/main/java/com/jamierf/powermeter/Reading.java
@@ -3,12 +3,18 @@ package com.jamierf.powermeter;
 
 public class Reading {
 
+	private int sensor;
 	private double temperature;
 	private int watts;
 
-	public Reading(double temperature, int watts) {
+	public Reading(int sensor, double temperature, int watts) {
+		this.sensor = sensor;
 		this.temperature = temperature;
 		this.watts = watts;
+	}
+
+	public int getSensor() {
+		return sensor;
 	}
 
 	public double getTemperature() {
@@ -21,6 +27,6 @@ public class Reading {
 
 	@Override
 	public String toString() {
-		return String.format("temp: %.1f, watts: %d", temperature, watts);
+		return String.format("sensor: %d, temp: %.1f, watts: %d", temperature, watts);
 	}
 }
